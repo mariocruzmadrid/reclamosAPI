2bb7c1e6107670ffc93a1445cc582f4809f81051
Mario <mariocruzmadrid96@gmail.com>
2020-04-30 03:47:38 +0200

    Proyecto creado con passport

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name'=>'required',
+            'email'=>'email|required',
+            'password'=>'required|confirmed'
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['user'=>$user, 'accessToken'=>$accessToken]);
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+           'email' => 'email|required',
+           'password' => 'required'
+        ]);
+
+        if(!auth()->attempt($loginData)) {
+            return response(['message'=>'Invalid credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'accessToken' => $accessToken]);
+    }
+
+    public function prueba()
+    {
+        return response(['message'=>'Hola']);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
