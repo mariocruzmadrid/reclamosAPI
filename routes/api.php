7d136e0bd17e8177b89b4a71c4c8aed631848970
Mario <mariocruzmadrid96@gmail.com>
2020-04-30 05:22:44 +0200

    controladores creados, rutas creadas y middleware auth implementado

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/register','Api\AuthController@register');
 Route::post('/login','Api\AuthController@login');
+Route::resource('/animals','Api\AnimalController')->middleware('auth:api');
+Route::resource('/reclamos','Api\ReclamoController')->middleware('auth:api');
+Route::resource('/registroAvistamientos','Api\RegistroAvistamientoController')->middleware('auth:api');
+Route::resource('/registroReclamos','Api\RegistroReclamosController')->middleware('auth:api');
+
 Route::get('/prueba','Api\AuthController@prueba')->middleware('auth:api');
